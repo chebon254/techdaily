8d2c8ffd967f1f40d76a3a1c563ccb70ca6108b9
Chebon kibet <kelvinchebon90@gmail.com>
2023-08-22 10:46:20 +0300

    Code Changes

---
+++ techdaily/js/script.js
@@ -0,0 +1,26 @@
+/* == BACK TO TOP == */
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+/* == BACK TO TOP == */
+
+
+/* == ANIMATION CSS == */
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 1;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("showreveal");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+/* == ANIMATION CSS == */
