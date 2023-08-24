3a999d719c52ebea22e732cce03a3023fe206835
Chebon kibet <kelvinchebon90@gmail.com>
2023-08-24 10:44:40 +0300

    Code Changes

--- techdaily/js/script.js
+++ techdaily/js/script.js
@@ -25,11 +25,23 @@ function reveal() {
 window.addEventListener("scroll", reveal);
 /* == ANIMATION CSS == */
 
+//Create A Responsive Menu Hide Show Side Menu
+const menuBtn = document.querySelector(".menu-btn");
+const cancelBtn = document.querySelector(".cancel-btn");
+const menu = document.querySelector(".links");
+
+// Onclick For Menu Bar
+menuBtn.onclick = () => {
+  menu.classList.add("active");
+  menuBtn.classList.add("hide");
+}
+
+// Onclick For Cancel Menu 
+cancelBtn.onclick = () => {
+  menu.classList.remove("active");
+  menuBtn.classList.remove("hide");
+}
 
 /* == BANNER SLIDER == */
 
 /* == BANNER SLIDER == */
-
-/* == EVENTS DROPDOWN == */
-
-/* == EVENTS DROPDOWN == */
